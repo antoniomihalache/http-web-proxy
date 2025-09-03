bcea68a07c4c091aa8674c604ff255eea7bad2d4
antonio <amihalache1@lenovo.com>
2025-09-03 16:15:10 +0300

    log request

--- server.js
+++ server.js
@@ -186,6 +186,7 @@ function connectHandler(req, clientSocket, head) {
     const host = lastColon > -1 ? req.url.slice(0, lastColon) : req.url;
     const portStr = lastColon > -1 ? req.url.slice(lastColon + 1) : '';
     const port = parseInt(portStr || '443', 10);
+    console.log('======================= req: ', req);
 
     log('CONNECT', `${host}:${port}`);
 
